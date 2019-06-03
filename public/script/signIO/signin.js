c5e342b342a65b6f41ac2853ff62eb6bf4a456d5
unknown <sonjh32@naver.com>
2019-06-03 17:34:10 +0900

    #

---
+++ public/script/signIO/signin.js
@@ -0,0 +1,10 @@
+// login function
+function login() {
+    // customer Id
+    var id = document.getElementById("customerId").value;
+    // customer password
+    var password = document.getElementById("customerPassword").value;
+
+    console.log(id + "!" + password);
+    
+}
